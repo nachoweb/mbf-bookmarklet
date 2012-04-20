e00ffcd36b5025b29ec89705f1bf59732282a2aa
Nachoweb <nachoelg@gmail.com>
2012-04-20 19:40:19 +0200

    estructura de ficheros

--- app/config-p.js
+++
@@ -1,5 +0,0 @@
-{
-  "id": "app",
-  "paths": ".",
-  "inputs": "app.js"
-}
---
+++ config-plovr.js
@@ -0,0 +1,10 @@
+{
+  // The id is used as a query parameter in the src of the <script> tag.
+  "id": "bookmarklet",
+  "paths": ".",
+  "inputs": "main.js",
+  "mode":"SIMPLE",
+  "level":"VERBOSE",
+  "css-inputs":"gss/style.gss",
+  "css-output-file":"style.css"
+}
